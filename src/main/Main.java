70065fb65c692373e17d83a2d8e5755ba81f5203
Saria <123633917+stsaria@users.noreply.github.com>
2023-03-06 08:36:18 +0000

    test2

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,10 @@ import etc.Check;
 
 class Main {
 	public static void main(String[] args) {
+		Check.run();
+
         double queryVersion = 0.1;
         String queryEdition = "alpha";
 		System.out.println("Autoer!\nQuery Version: "+String.valueOf(queryVersion)+"-"+queryEdition);
-
-		Check.run();
 	}
 }
