70d2c5fa5a6dafd340f78997f6abac32ecf70f82
Saria <123633917+stsaria@users.noreply.github.com>
2023-03-06 08:36:53 +0000

    test2

--- src/etc/Check.java
+++ src/etc/Check.java
@@ -1,14 +1,40 @@
 package etc;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.InetAddress;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.File;
 import java.net.URL;
 
 public class Check{
+    public static boolean file(String path, String attribute){
+        File file = new File(path);
+        if (!file.exists()) {
+            if (attribute == "file"){
+                try{
+                    Files.createFile(file.toPath());
+                } catch (IOException e){
+                    return false;
+                }
+                return true;
+            } else if (attribute == "dir"){
+                try{
+                    Files.createDirectory(file.toPath());
+                } catch (IOException e){
+                    return false;
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
     public static Object[] network() throws MalformedURLException{
         Object[] networkResult = new Object[3];
         // get globalip
@@ -47,15 +73,40 @@ public class Check{
         return networkResult;
     }
     public static void run(){
-        Object[] array;
+        System.out.print("Network ");
+        Object[] networkArray;
         try {
-            array = network();
-            boolean working = (boolean) array[0];
-            String privateIp = (String) array[1];
-            String globalIp = (String) array[2];
-            System.out.println("working: "+String.valueOf(working)+"\nGlobalIP: "+globalIp+"\nPrivateIP: "+privateIp);
+            for (int i = 0; i < 14; i ++){
+                System.out.print(".");
+                networkArray = network();
+                boolean working = (boolean) networkArray[0];
+                if (working){
+                    break;
+                }
+            }
+            networkArray = network();
+            boolean working = (boolean) networkArray[0];
+            if (working){
+                System.out.println(" OK");
+            } else {
+                System.out.println(" Error");
+                System.exit(1);
+            }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
+        System.out.print("File ");
+        String[] paths = {"data", "data/minecraft-list.txt", "data/minecraft-dir-list.txt", "minecraft"};
+        String[] attributes = {"dir", "file", "file", "dir"};
+        for (int i = 0; i < paths.length; i ++){
+            if (file(paths[i], attributes[i])){
+                System.out.print(".");
+            } else {
+                System.out.print("Error");
+                System.exit(2);
+            }
+        }
+        System.out.println(" OK");
+        System.out.println("Start!\n");
     }
 }
